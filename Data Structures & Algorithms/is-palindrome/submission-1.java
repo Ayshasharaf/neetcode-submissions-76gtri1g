283a1bd6882bb4c1f73d0b3bba980bf371e23b8c
Aysha Sharafudheen <155893699+Ayshasharaf@users.noreply.github.com>
2026-09-24 16:28:55 +0400

    Update: is-palindrome - submission-1

--- Data Structures & Algorithms/is-palindrome/submission-1.java
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -1,18 +1,20 @@
+
+
 class Solution {
-
     public boolean isPalindrome(String s) {
+        String cleaned=s.toLowerCase().replaceAll("[^a-z0-9]","");
+        int l=0;
+        int r = cleaned.length()-1;
+        while(l<r){
+            if(cleaned.charAt(l) != cleaned.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
 
-     s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-     int l = 0;
-     int r= s.length()-1;
-     while(l<r){
-        if (s.charAt(l)!=s.charAt(r)){
-            return false;
 
-        }
-        l++;
-        r--;
-     }return true;
+        }return true;
 
-    } 
+         
+    }
 }
